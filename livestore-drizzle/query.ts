e14112d4d208f86e2612c57f8f45cbea879897b2
Harry Solovay <harrysolovay@gmail.com>
2025-07-13 12:19:23 -0400

    refactor query to support non-selects

--- livestore-drizzle/query.ts
+++ livestore-drizzle/query.ts
@@ -1,26 +1,53 @@
 import type { Store } from "@livestore/livestore"
-import {
-  type AnySQLiteSelectQueryBuilder,
-  QueryBuilder,
-  SQLiteCustomColumn,
-  SQLiteSyncDialect,
-} from "drizzle-orm/sqlite-core"
+import type { ColumnsSelection } from "drizzle-orm"
+import { BaseSQLiteDatabase, SQLiteCustomColumn, SQLiteSyncDialect } from "drizzle-orm/sqlite-core"
 import * as Record from "effect/Record"
 
-export const query = <Built extends Pick<AnySQLiteSelectQueryBuilder, "_" | "toSQL">>(
-  store: Store<any, any>,
-  build: (qb: QueryBuilder) => Built,
-): Built["_"]["result"] => {
-  const built = build(new QueryBuilder(new SQLiteSyncDialect()))
-  const { sql, params } = built.toSQL()
-  const rows = store.query({
-    query: sql,
-    bindValues: params as never,
-  }) as Array<unknown>
-  return rows.map((row) =>
-    Record.map(built._.selectedFields, (col_, key) => {
-      const col = col_ as SQLiteCustomColumn<any>
-      return col.mapFromDriverValue((row as never)[key])
-    })
+export interface Query<T> {
+  sql: string
+  bindValues: Array<unknown>
+  run: (store: Store<any, any>) => Array<T>
+}
+
+export const query = <
+  Built extends {
+    _: {
+      result: Array<unknown>
+      selectedFields?: ColumnsSelection
+    }
+    toSQL: () => {
+      sql: string
+      params: Array<unknown>
+    }
+  },
+>(
+  build: (_: BaseSQLiteDatabase<"sync", any, any>) => Built,
+): Query<Built["_"]["result"]> => {
+  const mock = new BaseSQLiteDatabase(
+    "sync",
+    new SQLiteSyncDialect(),
+    null!,
+    null!,
   )
+  const built = build(mock)
+  const { sql, params } = built.toSQL()
+  return {
+    sql,
+    bindValues: params,
+    run: (store) =>
+      (store
+        .query({
+          query: sql,
+          bindValues: params as never,
+        }) as Array<unknown>)
+        .map((row) => {
+          const { selectedFields } = built._
+          return selectedFields
+            ? Record.map(selectedFields, (col_, key) => {
+              const col = col_ as SQLiteCustomColumn<any>
+              return col.mapFromDriverValue((row as never)[key])
+            })
+            : undefined
+        }) as never,
+  }
 }
